70028557abd2da8bc550e0e6a3bc31d5c981c1d2
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-09-27 17:07:17 -0300

    Feat: Add sqs updated queue (#29)
    * fix: update S3 bucket variable and add FIFO SQS queue for video status updates
    
    * fix: standardize formatting and improve readability in Makefile
    
    * fix: add video manager integration to API Gateway deployment

--- variables.tf
+++ variables.tf
@@ -97,6 +97,17 @@ variable "sqs_queues" {
     sns_topic_arn               = optional(string)
   }))
   default = {
+    "video-status-updated.fifo" = {
+      name                        = "video-status-updated.fifo"
+      delay_seconds               = 0
+      message_retention_seconds   = 1209600 # 14 days
+      visibility_timeout_seconds  = 60
+      fifo_queue                  = true
+      content_based_deduplication = true
+      tags = {
+        Purpose = "Receives events when video status is updated"
+      }
+    }
     "video-uploaded" = {
       name                        = "video-uploaded"
       delay_seconds               = 0
@@ -131,17 +142,17 @@ variable "sns_topics" {
   default = {
     "video-status-updated" = {
       name = "video-status-updated"
-      tags = { 
+      tags = {
         Purpose = "Sends a notification to users when a video has its status updated"
       }
     }
   }
 }
 
-variable "s3_bucket_video_processor_raw_videos" {
+variable "s3_bucket_video_processor" {
   description = "Map of S3 bucket configurations"
-  type = string
-  default = "fiapx-10soat-g21"
+  type        = string
+  default     = "fiapx-10soat-g21"
 }
 
 variable "lambda_user_service_memory" {
