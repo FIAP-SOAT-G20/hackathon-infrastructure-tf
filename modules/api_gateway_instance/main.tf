c30b6c66cfc0711bed5a9b50d44272074965d82a
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2025-09-28 17:44:11 -0500

    feat(api-gateway): add GET /users/{id} method in API Gateway configuration
    - Added `aws_api_gateway_method` for handling GET requests to `/users/{id}`.
    - Enabled request parameter `method.request.path.id` for dynamic user ID retrieval.

--- modules/api_gateway_instance/main.tf
+++ modules/api_gateway_instance/main.tf
@@ -126,6 +126,18 @@ resource "aws_api_gateway_method" "register_post" {
   authorization = "NONE"
 }
 
+# GET /users/{id}
+resource "aws_api_gateway_method" "user_get" {
+  rest_api_id   = aws_api_gateway_rest_api.hackathon_api.id
+  resource_id   = aws_api_gateway_resource.users_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
 resource "aws_api_gateway_integration" "users_register_integration" {
   rest_api_id = aws_api_gateway_rest_api.hackathon_api.id
   resource_id = aws_api_gateway_resource.register_resource.id
