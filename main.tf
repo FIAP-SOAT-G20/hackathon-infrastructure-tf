11407544b25c73f43892fea9545b3230ecb5ed0a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-09-28 10:13:21 -0300

    fix: add missing VPC CIDR and EKS security group on ElastiCache module (#31)

--- main.tf
+++ main.tf
@@ -77,14 +77,16 @@ module "alb_instance" {
 module "elasticache_instance" {
   source = "./modules/elasticache_instance"
 
-  project_name = var.project_name
-  environment  = var.environment
-  vpc_id       = module.eks_instance.vpc_id
-  subnet_ids   = module.eks_instance.private_subnet_ids
-  engine       = var.elasticache_engine
-  node_type    = var.elasticache_node_type
-  port         = var.elasticache_port
-  common_tags  = var.common_tags
+  project_name          = var.project_name
+  environment           = var.environment
+  vpc_id                = module.eks_instance.vpc_id
+  vpc_cidr_block        = module.eks_instance.vpc_cidr
+  eks_security_group_id = module.eks_instance.security_group_id
+  subnet_ids            = module.eks_instance.private_subnet_ids
+  engine                = var.elasticache_engine
+  node_type             = var.elasticache_node_type
+  port                  = var.elasticache_port
+  common_tags           = var.common_tags
 
   depends_on = [module.eks_instance]
 }
