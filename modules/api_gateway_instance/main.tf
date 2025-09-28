07ea54c9b95885aeeddf86d113fc4a0056332608
Alice Tomaz <tomaz.alice@gmail.com>
2025-09-28 20:53:04 -0300

    feat(api-gateway): add new integrations for user retrieval and dynamic user requests
    - Introduced `user_get_integration` for handling GET requests to retrieve user data.
    - Added `users_any_integration` to support dynamic user requests via the API Gateway.
    - Updated deployment configuration to include new integrations and methods.

--- modules/api_gateway_instance/main.tf
+++ modules/api_gateway_instance/main.tf
@@ -20,9 +20,10 @@ resource "aws_api_gateway_deployment" "hackathon_deployment" {
     aws_api_gateway_integration.users_register_integration,
     aws_api_gateway_integration.users_login_integration,
     aws_api_gateway_integration.users_me_integration,
+    aws_api_gateway_integration.users_any_integration,
+    aws_api_gateway_integration.user_get_integration,
     aws_api_gateway_integration.videos_any_integration,
     aws_api_gateway_integration.video_manager_any_integration,
-    aws_api_gateway_integration.users_proxy_integration,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.hackathon_api.id
@@ -37,8 +38,10 @@ resource "aws_api_gateway_deployment" "hackathon_deployment" {
       aws_api_gateway_resource.videos_resource.id,
       aws_api_gateway_resource.videos_manager_resource.id,
       aws_api_gateway_method.register_post.id,
+      aws_api_gateway_method.user_get.id,
       aws_api_gateway_method.login_post.id,
       aws_api_gateway_method.me_get.id,
+      aws_api_gateway_method.users_any.id,
       aws_api_gateway_method.videos_any.id,
       aws_api_gateway_method.videos_manager_any.id,
       aws_api_gateway_method.users_proxy_any.id,
@@ -46,9 +49,10 @@ resource "aws_api_gateway_deployment" "hackathon_deployment" {
       aws_api_gateway_integration.users_register_integration.id,
       aws_api_gateway_integration.users_login_integration.id,
       aws_api_gateway_integration.users_me_integration.id,
+      aws_api_gateway_integration.users_any_integration.id,
+      aws_api_gateway_integration.user_get_integration.id,
       aws_api_gateway_integration.videos_any_integration.id,
       aws_api_gateway_integration.video_manager_any_integration.id,
-      aws_api_gateway_integration.users_proxy_integration.id,
     ]))
   }
 
@@ -159,6 +163,16 @@ resource "aws_api_gateway_integration" "users_register_integration" {
   uri                     = var.lambda_invoke_arn
 }
 
+resource "aws_api_gateway_integration" "user_get_integration" {
+  rest_api_id = aws_api_gateway_rest_api.hackathon_api.id
+  resource_id = aws_api_gateway_resource.users_resource.id
+  http_method = aws_api_gateway_method.user_get.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
 # POST /users/login
 resource "aws_api_gateway_method" "login_post" {
   rest_api_id   = aws_api_gateway_rest_api.hackathon_api.id
@@ -212,6 +226,16 @@ resource "aws_api_gateway_method" "users_any" {
   resource_id   = aws_api_gateway_resource.users_resource.id
   http_method   = "ANY"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "users_any_integration" {
+  rest_api_id = aws_api_gateway_rest_api.hackathon_api.id
+  resource_id = aws_api_gateway_resource.users_resource.id
+  http_method = aws_api_gateway_method.users_any.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
 }
 
 # Users method
